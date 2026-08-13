86e1cc0f9bd2ecb31054529e0120838f36dbf19b
NFG Admin <admin@nfg.com>
2026-08-13 07:28:40 +0530

    Add kneepads.html to Vite build config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
         about: resolve(__dirname, 'about.html'),
         configurator: resolve(__dirname, 'configurator.html'),
         journal: resolve(__dirname, 'journal.html'),
-        helmets: resolve(__dirname, 'helmets.html')
+        helmets: resolve(__dirname, 'helmets.html'),
+        kneepads: resolve(__dirname, 'kneepads.html')
       }
     }
   }
